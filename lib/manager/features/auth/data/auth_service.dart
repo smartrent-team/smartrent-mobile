c2d6d6ac299c80aa7f31ed09a6f6e347162339f4
BacBT <nrowavip.30@gmail.com>
2026-05-27 15:35:55 +0700

    fix logix

--- lib/manager/features/auth/data/auth_service.dart
+++ lib/manager/features/auth/data/auth_service.dart
@@ -4,12 +4,12 @@ import 'package:smartrent_mobile/core/network/api_client.dart';
 class AuthService {
   final ApiClient _apiClient = ApiClient();
 
-  Future<Response> login(String email, String password) async {
+  Future<Response> login(String phone, String password) async {
     try {
       return await _apiClient.dio.post(
-        '/api/users/login',
+        '/api/auth/login',
         data: {
-          'email': email,
+          'phone': phone,
           'password': password,
         },
       );
